cb6bb23979028851fddb0b89e3d28a1f10c81fbb
mia-niznai <maria.popa.8735@gmail.com>
2022-12-22 20:59:54 +0200

    javascript pizza variables

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-Source:
+Sources:
+1.
 https://www.codecademy.com/courses/introduction-to-javascript/lessons/variables/exercises/var
+2. 
+https://food.codemoji.com/
--- data.js
+++ data.js
@@ -4,5 +4,42 @@ var favoriteFood = "pizza";
 //Create another variable with var
 var numOfSlices = 8;
 
+//Double number of slices
+numOfSlices *=2;
+console.log(numOfSlices)
+
 //Console log first variable
-console.log(favoriteFood);
+console.log(`My favorite food is ${favoriteFood}.`);
+
+//Name var pepperoni pizza using camelCase
+var pepperoniPizza;
+
+//Add another pizza
+let cheesePizza = 1+1;
+console.log(cheesePizza)
+
+//Substract pizza
+pepperoniPizza = 7-3;
+console.log(pepperoniPizza);
+
+//Multiply meat pizza
+var meatPizza = 4*2;
+console.log(meatPizza);
+
+//Create variable with your name as string
+var myName = "Mia";
+
+//Favorite pizza
+var iLove  = "I love";
+var pep = "pepperoni pizza";
+var haw = "hawaiian pizza";
+var che = "cheese pizza";
+favoritePizza = iLove + " " + che + ".";
+console.log(favoritePizza);
+
+//Move last pizza at the beginning of the pizza menu
+let pizzaMenu = ["pizza quattro stagioni", "pizza quattro formaggi", "pizza diavola", "pizza con tonno"]
+pizzaMenu.pop();
+console.log(pizzaMenu);
+pizzaMenu.unshift("pizza con tonno");
+console.log(pizzaMenu);
